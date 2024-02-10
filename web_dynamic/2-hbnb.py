c198316505ea587111e69538d3e7abb5da3f3765
aybzakaria5 <ayoub.don.zakaria@gmail.com>
2024-02-11 00:16:31 +0100

    task3

---
+++ web_dynamic/2-hbnb.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+""" Starts a Flash Web Application """
+from models import storage
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from os import environ
+from flask import Flask, render_template
+import uuid
+app = Flask(__name__)
+# app.jinja_env.trim_blocks = True
+# app.jinja_env.lstrip_blocks = True
+
+
+@app.teardown_appcontext
+def close_db(error):
+    """ Remove the current SQLAlchemy Session """
+    storage.close()
+
+
+# Change route to /2-hbnb
+@app.route('/2-hbnb', strict_slashes=False)
+def hbnb():
+    """ HBNB is alive! """
+    states = storage.all('State').values()
+    amenities = storage.all('Amenity').values()
+    places = storage.all('Place').values()
+    users = storage.all('User')
+    places_list = []
+    cache_id = uuid.uuid4()
+    for k, v in users.items():
+        for place in places:
+            if k == place.user_id:
+                places_list.append(["{} {}".format(v.first_name, v.last_name),
+                                   place])
+    places_list.sort(key=lambda x:[1].name)
+    return render_template('2-hbnb.html',
+                           states=states,
+                           users=users,
+                           amenities=amenities,
+                           places=places,
+                           cache_id=cache_id)
+
+
+if __name__ == "__main__":
+    """ Main Function """
+    app.run(debug=True, host='0.0.0.0', port=5000)
---
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -0,0 +1,21 @@
+$(document).ready(function () {
+    const amenityId = {};
+    $('.amenities .popever li input').change(function (event) {
+      if (this.checked) {
+        amenityId[this.dataset.id] = this.dataset.name;
+      } else {
+        delete amenityId[this.dataset.id];
+      }
+      const names = Object.values(amenityId);
+      $('.amenities h4').text(names.join(', '));
+    });
+  
+    $.get('http://0.0.0.0:5001/api/v1/status/', function (data) {
+      if (data.status === 'OK') {
+        $('#api_status').addClass('available');
+      } else {
+        $('#api_status').removeClass('available');
+      }
+    });
+  });
+  
