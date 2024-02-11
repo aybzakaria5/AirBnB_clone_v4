e47b06422ddd2adea8ad75702a315489d6a345e4
Lelaabk <lolabkari@gmail.com>
2024-02-11 15:01:15 -0800

    Fix lines

--- web_dynamic/static/scripts/4-hbnb.js
+++ web_dynamic/static/scripts/4-hbnb.js
@@ -32,38 +32,36 @@ $(document).ready(function () {
     });
 });
 function ListofPlaces (amenities = '{}') {
-    $.post({
-		url: "http://0.0.0.0:5001/api/v1/places_search",
-		data: JSON.stringify({}),
-		headers: {
-			"Content-Type": "application/json",
-		},
-		success: (data) => {
-			data.forEach((place) =>
-				$("section.places").append(
-					`<article>
-			<div class="title_box">
-			<h2>${place.name}</h2>
-			<div class="price_by_night">$${place.price_by_night}</div>
-			</div>
-			<div class="information">
-			<div class="max_guest">${place.max_guest} Guest${
-						place.max_guest !== 1 ? "s" : ""
-					}</div>
-			<div class="number_rooms">${place.number_rooms} Bedroom${
-						place.number_rooms !== 1 ? "s" : ""
-					}</div>
-			<div class="number_bathrooms">${place.number_bathrooms} Bathroom${
-						place.number_bathrooms !== 1 ? "s" : ""
-					}</div>
-			</div> 
-			<div class="description">
-			${place.description}
-			</div>
-				</article>`
-				)
-			);
-		},
-		dataType: "json",
-	});
+  $.ajax({
+    type:'POST',
+    url: "http://0.0.0.0:5001/api/v1/places_search",
+    dataType: 'json',
+    data: JSON.stringify({}),
+    contentType: 'application/json; charset=utf-8',
+    success: function (places) {
+      for (let i = 0; i < places.length; i++) {
+        $('section.places').append(
+	`<article>
+	<div class="title_box">
+	<h2>${place[i].name}</h2>
+	<div class="price_by_night">$${place[i].price_by_night}</div>
+	</div>
+	<div class="information">
+	<div class="max_guest">${place[i].max_guest} Guest${
+	place[i].max_guest !== 1 ? "s" : ""
+	}</div>
+	<div class="number_rooms">${place[i].number_rooms} Bedroom${
+	place[i].number_rooms !== 1 ? "s" : ""
+	}</div>
+	<div class="number_bathrooms">${place[i].number_bathrooms} Bathroom${
+	place[i].number_bathrooms !== 1 ? "s" : ""
+	}</div>
+	</div> 
+	<div class="description">
+	${place[i].description}
+	</div>
+	</article>`
+	);
+      }
+  });
 }
